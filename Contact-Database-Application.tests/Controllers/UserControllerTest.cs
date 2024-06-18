e5c9f86d4cd9df8a2c102a0d61bbfc9c0dc8be92
github_cloudlabsuser_2094 <github_cloudlabsuser_2094@cloudlabsaiuser.com>
2024-06-18 14:42:12 +0000

    Moved unit test project to the solution folder

---
+++ Contact-Database-Application.tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CRUD_application_2.Controllers;
+using CRUD_application_2.Models;
+using System.Web.Mvc;
+
+namespace CRUD_application_2.Tests.Controllers
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+        UserController controller = new UserController();
+        User testUser = new User { Id = 1, Name = "Test User", Email = "test@example.com" };
+
+        [TestMethod]
+        public void Index()
+        {
+			// Arrange
+			UserController.userlist.Add(testUser);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            UserController.userlist.Add(testUser);
+        
+            // Act
+            ViewResult result = controller.Details(1) as ViewResult;
+            User resultUser = result.Model as User;
+        
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testUser.Id, resultUser.Id);
+            Assert.AreEqual(testUser.Name, resultUser.Name);
+            Assert.AreEqual(testUser.Email, resultUser.Email);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            User newUser = new User { Id = 2, Name = "New User", Email = "new@example.com" };
+
+            // Act
+            RedirectToRouteResult result = controller.Create(newUser) as RedirectToRouteResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+			// Arrange
+			UserController.userlist.Add(testUser);
+            User updatedUser = new User { Id = 1, Name = "Updated User", Email = "updated@example.com" };
+
+            // Act
+            RedirectToRouteResult result = controller.Edit(1, updatedUser) as RedirectToRouteResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+			// Arrange
+			UserController.userlist.Add(testUser);
+
+            // Act
+            RedirectToRouteResult result = controller.Delete(1, new FormCollection()) as RedirectToRouteResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void Search()
+        {
+			// Arrange
+			UserController.userlist.Add(testUser);
+
+            // Act
+            ViewResult result = controller.Search("Test") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
---
+++ Contact-Database-Application.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a378f34-8993-40b9-b231-942897ad6383")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
